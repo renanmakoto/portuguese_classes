bbebee4ec6323dedee633aae81cbaab62bb1f5fd
renan <renanmakoto@outlook.com>
2025-02-25 13:14:49 -0500

    dia 3 route fixed

--- dia3/script.js
+++ dia3/script.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const files = [
         { name: "📊 Slides - Aula 3", url: "./fev-10.pptx", icon: "fas fa-file-powerpoint" },
-        { name: "📝 Documentos - Aula 3", url: "fev-10-licao-casa.docx", icon: "fas fa-file-word" }
-    ];
+        { name: "📝 Documentos - Aula 3", url: "./fev-10-licao-casa.docx", icon: "fas fa-file-word" }
+    ]
 
     files.forEach(file => {
-        const li = document.createElement("li");
-        li.innerHTML = `<i class="${file.icon}"></i> <a href="${file.url}" download>${file.name}</a>`;
-        fileList.appendChild(li);
-    });
-});
+        const li = document.createElement("li")
+        li.innerHTML = `<i class="${file.icon}"></i> <a href="${file.url}" download>${file.name}</a>`
+        fileList.appendChild(li)
+    })
+})
