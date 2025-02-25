917d7b467f2550382fbe3551cf4128a027d687be
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-02-25 08:37:07 -0500

    Update script.js

--- dia4/script.js
+++ dia4/script.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const fileList = document.getElementById("file-list");
 
     const files = [
-        { name: "📊 Slides - Aula 4", url: "files/slides1.pptx", icon: "fas fa-file-powerpoint" },
-        { name: "📝 Documentos - Aula 4", url: "files/notas1.docx", icon: "fas fa-file-word" }
+        { name: "📊 Slides - Aula 4", url: "./fev-12.pptx", icon: "fas fa-file-powerpoint" },
+        { name: "📝 Documentos - Aula 4", url: "./fev-12.pdf", icon: "fas fa-file-word" }
     ];
 
     files.forEach(file => {
