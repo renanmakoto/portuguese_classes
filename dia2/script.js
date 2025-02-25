6d5a6408be3da62243d03496a4f16082ddf65b51
renan <renanmakoto@outlook.com>
2025-02-25 08:27:59 -0500

    dia2 fixing

--- dia2/script.js
+++ dia2/script.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const fileList = document.getElementById("file-list")
 
     const files = [
-        { name: "📊 Slides - Aula 2", url: "files/slides1.pptx", icon: "fas fa-file-powerpoint" },
-        { name: "📝 Documentos - Aula 2", url: "files/notas1.docx", icon: "fas fa-file-word" }
+        { name: "📊 Slides - Aula 2", url: "./fev-05.pptx", icon: "fas fa-file-powerpoint" },
+        { name: "📝 Documentos - Aula 2", url: "./menu.pdf", icon: "fas fa-file-word" }
     ]
 
     files.forEach(file => {
