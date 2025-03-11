c62217312428ef8766886200ccc944a4e822d1fa
renan <renanmakoto@outlook.com>
2025-03-11 13:01:00 -0400

    Aula 10 Files added

--- aula_10/script.js
+++ aula_10/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const fileList = document.getElementById("file-list")
 
     const files = [
-        { name: "📊 Slides - Aula 10", url: "", icon: "fas fa-file-powerpoint" },
+        { name: "📊 Slides - Aula 10", url: "./mar-10.pptx", icon: "fas fa-file-powerpoint" },
     ]
 
     files.forEach(file => {
